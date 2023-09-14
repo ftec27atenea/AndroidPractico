a94a0edb2e460be97b9b39183814b8cc47814743
guakero <116929201+guakero@users.noreply.github.com>
2023-09-13 20:13:21 -0500

    recycle view

--- app/src/main/java/com/example/myapplication/Ejercicio_septiembre_12.java
+++ app/src/main/java/com/example/myapplication/Ejercicio_septiembre_12.java
@@ -1,14 +1,18 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class Ejercicio_septiembre_12 extends AppCompatActivity {
-
+private RecyclerView.Adapter adapter;
+private RecyclerView recyclerViewList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio_septiembre12);
+
+        recyclerViewList=findViewById(R.id.view);
     }
 }
